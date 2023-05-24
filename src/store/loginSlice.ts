d25317513e8d564a511835be5f3ca97754137939
Julien Wajsberg <felash@gmail.com>
2023-05-24 21:30:17 +0200

    Add redux and prettier

---
+++ src/store/loginSlice.ts
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { RootState } from ".";
+
+export const loginSlice = createSlice({
+  name: "login",
+  initialState: { value: "" },
+  reducers: {
+    setLogin: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+export const { setLogin } = loginSlice.actions;
+export const getLogin = (state: RootState) => state.login.value;
+export default loginSlice.reducer;
