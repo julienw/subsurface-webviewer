3bf1876a331e0307f28c5667a85104331d15ed92
Julien Wajsberg <felash@gmail.com>
2023-06-10 17:03:54 +0200

    First version with working graphs

--- src/DiveList.tsx
+++ src/DiveList.tsx
@@ -1,20 +1,54 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Localized } from "@fluent/react";
+import "chart.js/auto";
+import { Line } from "react-chartjs-2";
 import { type RootState } from "./store";
 import type { Trip, Dive } from "./types";
 import "./DiveList.css";
+
+function DiveGraph({ dive }: { dive: Dive }) {
+  return (
+    <div>
+      <Line
+        data={{
+          datasets: [
+            {
+              data: dive.samples.map(([time, depth]) => ({ time, depth })),
+            },
+          ],
+        }}
+        options={{
+          parsing: {
+            xAxisKey: "time",
+            yAxisKey: "depth",
+          },
+          scales: {
+            y: {
+              type: "linear",
+              reverse: true,
+            },
+            x: { type: "linear" },
+          },
+        }}
+      />
+    </div>
+  );
+}
 
 function Dive({ trip, dive }: { trip: Trip; dive: Dive }) {
   const [shown, toggleShow] = useState(false);
 
   return (
-    <label className="dive-line">
-      {trip.name}: ({dive.number}) {dive.location}
-      <button type="button" onClick={() => toggleShow(!shown)}>
-        <Localized id={shown ? "hide-dive" : "show-dive"} />
-      </button>
-    </label>
+    <>
+      <label className="dive-line">
+        {trip.name}: ({dive.number}) {dive.location}
+        <button type="button" onClick={() => toggleShow(!shown)}>
+          <Localized id={shown ? "hide-dive" : "show-dive"} />
+        </button>
+      </label>
+      {shown ? <DiveGraph dive={dive} /> : null}
+    </>
   );
 }
 
@@ -25,7 +59,7 @@ function TripInfo({ trip }: { trip: Trip }) {
         .slice()
         .reverse()
         .map((dive) => (
-          <Dive trip={trip} dive={dive} />
+          <Dive key={dive.number} trip={trip} dive={dive} />
         ))}
     </>
   );
@@ -39,8 +73,8 @@ export function DiveList() {
       {dataInformation.data
         ?.slice()
         .reverse()
-        .map((trip) => (
-          <TripInfo trip={trip} />
+        .map((trip, index) => (
+          <TripInfo trip={trip} key={index} />
         ))}
     </>
   );
