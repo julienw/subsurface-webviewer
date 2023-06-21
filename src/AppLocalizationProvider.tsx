52128c125a839b51755a55c016be136488d6388f
Julien Wajsberg <felash@gmail.com>
2023-06-21 10:25:56 +0200

    Use the MPL 2 license

--- src/AppLocalizationProvider.tsx
+++ src/AppLocalizationProvider.tsx
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 import { Children, useEffect, useState } from "react";
 import { type ReactNode } from "react";
 import { FluentBundle, FluentResource } from "@fluent/bundle";
--- src/DataLoader.tsx
+++ src/DataLoader.tsx
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 import { useEffect } from "react";
 import { useAppSelector, useAppDispatch } from "./store/hooks";
 import { fetchDataForUser } from "./store/dataSlice";
