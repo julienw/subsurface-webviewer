791636f3f3f52ca0791027a504951b09e23a31bf
Julien Wajsberg <felash@gmail.com>
2023-07-26 15:40:51 +0200

    Do not show the hello line before login is successful

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import { useSelector } from "react-redux";
 import { Localized } from "@fluent/react";
+import { useAppSelector } from "./store/hooks";
 import { Login } from "./Login";
 import { DiveList } from "./DiveList";
-import type { RootState } from "./types";
 import "./App.css";
 
 function App() {
-  const user = useSelector((state: RootState) => state.login?.user);
+  const user = useAppSelector((state) => state.login.user);
+  const loadingState = useAppSelector((state) => state.data.loading);
 
   return (
     <div className="App">
@@ -21,7 +21,9 @@ function App() {
       </h1>
       <Localized id="welcome-to-subsurface" />
       <Login />
-      {user ? <Localized id="hello-user" vars={{ user }} /> : null}
+      {loadingState === "succeeded" && user ? (
+        <Localized id="hello-user" vars={{ user }} />
+      ) : null}
       <DiveList />
     </div>
   );
