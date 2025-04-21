bad74caa894173352b4624879f355ccf0a506575
Julien Wajsberg <felash@gmail.com>
2025-04-21 11:30:38 +0200

    Add polyfills for Chrome and Webkit

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -4,6 +4,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+// Chrome
 if (!new Uint8Array().toBase64) {
   Uint8Array.prototype.toBase64 = function (
     options: Partial<{
@@ -26,4 +27,55 @@ if (!new Uint8Array().toBase64) {
 
     return encodedStr;
   };
+}
+
+// Chrome
+if (!Uint8Array.fromBase64) {
+  Uint8Array.fromBase64 = function (
+    base64: string,
+    options: Partial<{
+      alphabet: "base64" | "base64url";
+      lastChunkHandling: "loose" | "strict" | "stop-before-partial";
+    }> = {},
+  ): Uint8Array {
+    if (options.alphabet === "base64url") {
+      base64 = base64.replaceAll("-", "+").replaceAll("_", "/");
+    }
+    const decoded = atob(base64);
+    const result = new Uint8Array(decoded.length);
+    for (let i = 0; i < result.length; i++) {
+      result[i] = decoded.charCodeAt(i);
+    }
+
+    return result;
+  };
+}
+
+// Safari
+if (!ReadableStream.prototype.values) {
+  ReadableStream.prototype.values = async function* <T>(
+    this: ReadableStream<T>,
+    options: Partial<{ preventCancel: boolean }> = {},
+  ): ReadableStreamAsyncIterator<T> {
+    let done = false;
+    let value = null;
+    const reader = this.getReader();
+    try {
+      while (!done) {
+        ({ done, value } = await reader.read());
+        if (value !== undefined) yield value;
+      }
+    } finally {
+      reader.releaseLock();
+      if (!options.preventCancel) {
+        await reader.cancel();
+      }
+    }
+  };
+}
+
+// Safari
+if (!ReadableStream.prototype[Symbol.asyncIterator]) {
+  ReadableStream.prototype[Symbol.asyncIterator] =
+    ReadableStream.prototype.values;
 }
